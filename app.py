f54537864346e375b6615ad073f02feb2fffaa94
Jiayang Dong <dongjiayang123@hotmail.com>
2025-09-27 21:23:06 -0700

    added texlive extra and fonts

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ import uuid
 # s3_client = boto3.client('s3')
 
 def handler(event, context):
+    print("log log!!!")
     try:
         latex_source = event.get('latex_source')
         if not latex_source:
             return {
-                'status': 400,
-                'body': 'Missing "latex_source in the event payload!"'
+                'statusCode': 400,
+                'body': 'Missing "latex_source" in the event payload!'
             }
         
         working_dir = '/tmp'
@@ -25,21 +26,29 @@ def handler(event, context):
         base_filename = f'document_{unique_id}'
         tex_file_path = os.path.join(working_dir, f'{base_filename}.tex')
         pdf_file_path = os.path.join(working_dir, f'{base_filename}.pdf')
+        log_path = os.path.join(working_dir, f'{base_filename}.log')
         
         with open(tex_file_path, 'w') as f:
             f.write(latex_source)
 
         for _ in range(2):
             process = subprocess.run(
-                ['pdflatex', '-interaction=nonstopmode', '-output-directory=' + working_dir, tex_file_path],
+                ['pdflatex', '-interaction=nonstopmode','-halt-on-error', '-file-line-error', '-output-directory=' + working_dir, tex_file_path],
                 capture_output=True,
                 text=True
             )
+
+        print(f"[pdf-latex stdout head]\n{process.stdout[:1000]}")
+        print(f"[pdf-latex stderr head]\n{process.stderr[:500]}")
         
         if process.returncode != 0:
+            if os.path.exists(log_path):
+                with open(log_path, 'r', errors="ignore") as log_file:
+                    log_text = log_file.read()
+                print("Latex compilation error!\n[pdf-latex log tail]\n" + log_text[-2000:])
             return {
                 'statusCode': 500,
-                'body': f"Latex compilation failed. Error: {process.stderr}"
+                'body': f"Latex compilation error: {process.stderr}"
             }
 
         # # upload to s3
@@ -57,6 +66,7 @@ def handler(event, context):
 
         with open(pdf_file_path, "rb") as pdf_file:
             encoded_string = base64.b64encode(pdf_file.read()).decode()
+        print(f"Successfully compiled to pdf!")
         return {
             'statusCode': 200,
             'headers': {
@@ -67,7 +77,8 @@ def handler(event, context):
         }
         
     except Exception as e:
+        print(f"Unexpected error: {str(e)}")
         return {
             'statusCode': 500,
-            'body': f"Uxpected error: {str(e)}"
+            'body': f"Unexpected error: {str(e)}"
         }
